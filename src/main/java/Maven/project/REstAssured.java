762456125a50163485b5381253b74d654e58cf61
Aarti <artivanam1993@gmail.com>
2022-04-19 15:16:52 +0530

    First Commit message

---
+++ src/main/java/Maven/project/REstAssured.java
@@ -0,0 +1,18 @@
+package Maven.project;
+
+import org.testng.annotations.Test;
+
+public class REstAssured {
+	@Test
+	public void PostJIRA()
+	{
+		System.out.println("PostJIRA");
+	}
+
+
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+	}
+}
---
+++ src/main/java/Maven/project/seleniumtest.java
@@ -0,0 +1,18 @@
+package Maven.project;
+
+import org.testng.annotations.Test;
+
+public class seleniumtest {
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+
+
+	@Test
+	public void ElementuI()
+	{
+		System.out.println("ElementuI");
+	}
+}
